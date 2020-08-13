699eea830dbdee659592dace601454167bf14dab
Abhi Jain <devabhixda@gmail.com>
2020-08-13 22:05:33 +0530

    Make world clock section functional

---
+++ lib/TimeZones.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:ozoneclock/consts.dart';
+
+class TimeZones extends StatefulWidget {
+  final tzone;
+  TimeZones({@required this.tzone});
+  @override
+  _TimeZonesState createState() => _TimeZonesState();
+}
+
+class _TimeZonesState extends State<TimeZones> {
+  var filtered = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "World",
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () => {Navigator.pop(context, "")},
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                fillColor: base,
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(40)),
+                labelText: "Search",
+                suffixIcon: Icon(Icons.search),
+              ),
+              onChanged: (text) {
+                setState(() {
+                  filtered = widget.tzone.keys
+                      .where((r) => (r
+                          .toString()
+                          .toLowerCase()
+                          .contains(text.toString().toLowerCase())))
+                      .toList();
+                });
+              },
+            ),
+          ),
+          filtered.length == 0
+              ? Expanded(
+                  child: ListView.builder(
+                  padding: EdgeInsets.all(16.0),
+                  itemCount: widget.tzone.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var curr = DateTime.now().add(widget.tzone.values.elementAt(index));
+                    return Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Container(
+                            padding: EdgeInsets.all(5),
+                            child: OutlineButton(
+                                onPressed: () => {
+                                      Navigator.pop(context,
+                                          widget.tzone.keys.elementAt(index))
+                                    },
+                                padding: EdgeInsets.all(2),
+                                child: ListTile(
+                                    title: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(widget.tzone.keys
+                                            .elementAt(index)
+                                            .toString()),
+                                        SizedBox(height: 5),
+                                        Text(
+                                          DateFormat('EEE d MMM').format(curr),
+                                          style: TextStyle(fontSize: 10),
+                                        )
+                                      ],
+                                    ),
+                                    trailing: Text(
+                                        DateFormat('h:mm a').format(curr))),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)))));
+                  },
+                ))
+              : Expanded(
+                  child: ListView.builder(
+                  padding: EdgeInsets.all(16.0),
+                  itemCount: filtered.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var curr = DateTime.now().add(widget.tzone[filtered.elementAt(index)]);
+                    return Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Container(
+                            padding: EdgeInsets.all(5),
+                            child: OutlineButton(
+                                onPressed: () => {
+                                      Navigator.pop(context,
+                                          filtered.elementAt(index).toString())
+                                    },
+                                padding: EdgeInsets.all(2),
+                                child: ListTile(
+                                    title: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(filtered
+                                            .elementAt(index)
+                                            .toString()),
+                                        SizedBox(height: 5),
+                                        Text(
+                                          DateFormat('EEE d MMM').format(curr),
+                                          style: TextStyle(fontSize: 10),
+                                        )
+                                      ],
+                                    ),
+                                    trailing: Text(
+                                        DateFormat('h:mm a').format(curr))),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30)))));
+                  },
+                ))
+        ],
+      ),
+    );
+  }
+}
--- lib/World.dart
+++ lib/World.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'package:analog_clock/analog_clock.dart';
 import 'package:flutter/material.dart';
+import 'package:ozoneclock/TimeZones.dart';
 import 'package:ozoneclock/consts.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
+import 'package:sortedmap/sortedmap.dart';
+import 'package:intl/intl.dart';
 
 class World extends StatefulWidget {
   @override
@@ -12,96 +15,189 @@ class World extends StatefulWidget {
 
 class _WorldState extends State<World> {
   DateTime curr = DateTime.now();
-  var tzones = {};
+  var tzones = new SortedMap(Ordering.byKey());
+  var times = [];
   void initState() {
     super.initState();
-    Duration updateDuration = Duration(minutes: 1);
+    curr = DateTime.now();
+    Duration updateDuration = Duration(seconds: 1);
     Timer.periodic(updateDuration, update);
     tz.initializeTimeZones();
     var locations = tz.timeZoneDatabase.locations;
     for (String temp in locations.keys) {
       var loc = tz.getLocation(temp);
-      tzones[loc.toString()] = tz.TZDateTime.from(curr, loc);
+      int index = loc.toString().indexOf('/');
+      int idx = 0;
+      while (index >= 0) {
+        idx = index;
+        index = loc.toString().indexOf('/', index + 1);
+      }
+      tzones[loc.toString().substring(idx + 1)] =
+          (tz.TZDateTime.from(curr, loc).timeZoneOffset);
     }
   }
 
   update(Timer timer) {
-      // update is only called on live clocks. So, it's safe to update datetime.
-      curr = DateTime.now();
-      setState(() {});
+    // update is only called on live clocks. So, it's safe to update datetime.
+    curr = DateTime.now();
+    //print(curr);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.transparent,
         body: Center(
             child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Container(
-            height: 200,
-            child: Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(200))),
-              child: AnalogClock(
-                width: 200.0,
-                isLive: true,
-                hourHandColor: Colors.black,
-                minuteHandColor: Colors.black,
-                secondHandColor: base,
-                showSecondHand: true,
-                numberColor: Colors.white,
-                showNumbers: true,
-                textScaleFactor: 1.4,
-                showTicks: false,
-                showDigitalClock: false,
-                datetime: curr,
-              ),
-            )),
-        SizedBox(
-          height: 40,
-        ),
-        Text(
-          "Current Timezone- " + curr.timeZoneName,
-          style: TextStyle(
-            fontFamily: 'Segoe UI',
-            fontSize: 14,
-            color: const Color(0x8f1f2426),
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              curr.hour.toString() +
-                  ":" +
-                  (curr.minute.toInt() < 10 ? "0" : "") +
-                  curr.minute.toString(),
-              style: TextStyle(
-                fontFamily: 'Segoe UI',
-                fontSize: 48,
-                color: const Color(0xff1f2426),
-                fontWeight: FontWeight.w600,
-              ),
+            Container(
+                height: 200,
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(200))),
+                  child: AnalogClock(
+                    width: 200.0,
+                    isLive: true,
+                    hourHandColor: Colors.black,
+                    minuteHandColor: Colors.black,
+                    secondHandColor: base,
+                    showSecondHand: true,
+                    numberColor: Colors.white,
+                    showNumbers: true,
+                    textScaleFactor: 1.4,
+                    showTicks: false,
+                    showDigitalClock: false,
+                    datetime: curr,
+                  ),
+                )),
+            SizedBox(
+              height: 40,
             ),
             Text(
-              (curr.hour.toInt() < 12 ? " AM" : " PM"),
+              "Current Timezone- " + curr.timeZoneName,
               style: TextStyle(
                 fontFamily: 'Segoe UI',
-                fontSize: 24,
+                fontSize: 14,
                 color: const Color(0x8f1f2426),
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w400,
               ),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  DateFormat('hh:mm:ss').format(curr),
+                  style: TextStyle(
+                    fontFamily: 'Segoe UI',
+                    fontSize: 48,
+                    color: const Color(0xff1f2426),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  DateFormat(' a').format(curr),
+                  style: TextStyle(
+                    fontFamily: 'Segoe UI',
+                    fontSize: 24,
+                    color: const Color(0x8f1f2426),
+                    fontWeight: FontWeight.w600,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            OutlineButton(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 100),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              child: Text(
+                "+ ADD NEW",
+                style: TextStyle(
+                  fontFamily: 'Segoe UI',
+                  fontSize: 18,
+                  color: const Color(0xff2680eb),
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              onPressed: () => {_navigateAndDisplaySelection(context)},
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            times.length > 0
+                ? Expanded(
+                    child: ListView.builder(
+                    padding: EdgeInsets.only(left: 16, right: 16, bottom: 100),
+                    itemCount: times.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var loc = curr.add(tzones[times.elementAt(index)]);
+                      return Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Container(
+                              padding: EdgeInsets.all(5),
+                              child: OutlineButton(
+                                  padding: EdgeInsets.all(2),
+                                  child: ListTile(
+                                      title: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            times.elementAt(index).toString(),
+                                            style: TextStyle(
+                                              fontFamily: 'Segoe UI',
+                                              fontSize: 14,
+                                              color: const Color(0xff1f2426),
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            DateFormat('EEE d MMM').format(loc),
+                                            style: TextStyle(
+                                              fontFamily: 'Segoe UI',
+                                              fontSize: 10,
+                                              color: const Color(0xff1f2426),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      trailing: Text(
+                                        DateFormat('h:mm a').format(loc),
+                                        style: TextStyle(
+                                          fontFamily: 'Segoe UI',
+                                          fontSize: 16,
+                                          color: const Color(0xff1f2426),
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      )),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(30)))));
+                    },
+                  ))
+                : Container()
           ],
-        )
-      ],
-    )));
+        )));
+  }
+
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that completes after calling
+    // Navigator.pop on the Selection Screen.
+    var result = await Navigator.push(
+      context,
+      // Create the SelectionScreen in the next step.
+      MaterialPageRoute(builder: (context) => TimeZones(tzone: tzones)),
+    );
+    if (result != null && !times.contains(result)) times.add(result);
   }
 }
