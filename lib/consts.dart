81eb8c35222e346103b8f2a4756a1bade1abb768
Abhi Jain <devabhixda@gmail.com>
2020-08-15 11:30:56 +0530

    Define more theme colors

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,2 +1,4 @@
 import 'package:flutter/material.dart';
-Color base = Color(0xff2680eb); 
+Color base = Color(0xff2680eb);
+Color kindaGray = Color(0x8f1f2426);
+Color notSoBlack = Color(0xff1f2426); 
